da8d4ccca2d2ef1b65b161254f6c56ce07f588e3
sudoleonidb <sudobadeevleonid@gamil.com>
2023-08-21 14:54:23 +0200

    cqr

---
+++ src/main/java/org/example/cqr/AddUserCommand.java
@@ -0,0 +1,52 @@
+package org.example.cqr;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Класс "AddUserCommand".
+ * Реализует добавление пользователя в базу данных.
+ * Реализует интерфейс Command.
+ */
+public class AddUserCommand implements Command {
+
+    // Соединение с базой данных
+    private final Connection connection;
+    // Имя пользователя
+    private final String userName;
+
+    /**
+     * Конструктор.
+     *
+     * @param connection Соединение с базой данных.
+     * @param userName Имя пользователя для добавления.
+     */
+    public AddUserCommand(Connection connection, String userName) {
+        this.connection = connection;
+        this.userName = userName;
+    }
+
+    /**
+     * Выполнить добавление пользователя в базу данных.
+     */
+    @Override
+    public void execute() {
+        String sql = "INSERT INTO users (name) VALUES (?)";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, userName);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+//Конструктор: принимает соединение с базой данных и имя
+// пользователя для добавления.
+//
+//Метод execute: выполняет добавление пользователя
+// в таблицу users базы данных. Для этого используется SQL
+// запрос INSERT INTO, и данные добавляются с помощью
+// PreparedStatement, чтобы предотвратить SQL инъекции
